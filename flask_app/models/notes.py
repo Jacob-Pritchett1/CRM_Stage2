f0c34ab34025a110d2b6f7bfca6ea507d0df581a
Jacob Pritchett <117482972+Jacob-Pritchett1@users.noreply.github.com>
2023-10-20 16:56:57 -0500

    Update notes.py
    Changes to methods

--- flask_app/models/notes.py
+++ flask_app/models/notes.py
@@ -3,18 +3,19 @@ from flask import flash
 from flask_app.models.user import User
 
 
-class Post:
+class Note:
     DB = "users"
     def __init__(self,data):
         self.id= data['id']
-        self.date=data['data']
+        self.note=data['note']
+        self.date=data['date']
         self.created_at=data['created_at']
         self.updated_at=data['updated_at']
         self.user_id= None
-    @classmethod # Create a new post in the DB
+    @classmethod # Create a new note in the DB
     def create_note(cls, data):
-        query = """ INSERT INTO notes(note, created_at, updated_at, user_id)
-        VALUES (%(note)s, NOW(), NOW(), %(user_id)s);"""
+        query = """ INSERT INTO note(note,data, created_at, updated_at, user_id)
+        VALUES (%(note)s, %(date)s,NOW(), NOW(), %(user_id)s);"""
         return connectToMySQL('users').query_db(query, data)
     @staticmethod 
     def validate_note(note):
@@ -32,7 +33,7 @@ class Post:
             one_note.append(cls(row))
         return one_note
 
-    @classmethod # get all posts
+    @classmethod # get all notes
     def get_all_notes(cls):
         query= "SELECT * from note"
         results = connectToMySQL(cls.DB).query_db(query)
@@ -42,16 +43,16 @@ class Post:
         return all_notes
     @classmethod
     def edit_note(cls, data):
-        query = """ UPDATE notes SET note=%(note)s WHERE id = %(id)s;"""
+        query = """ UPDATE note SET note=%(note)s WHERE id = %(id)s;"""
         return connectToMySQL(cls.DB).query_db(query, data)
     @classmethod
     def delete(cls, id):
-        query = "DELETE FROM notes WHERE id = %(id)s;"
+        query = "DELETE FROM note WHERE id = %(id)s;"
         data = {"id": id}
         return connectToMySQL(cls.DB).query_db(query,data)
     @classmethod
     def users_notes(cls, data):
-        query= "SELECT * from user LEFT JOIN notes ON note.user_id = user.id WHERE user.id = %(id)s;"
+        query= "SELECT * from user LEFT JOIN note ON note.user_id = user.id WHERE user.id = %(id)s;"
         results= connectToMySQL('users').query_db(query, data)
         print(results)
         user_ = User.User(results[0])
@@ -59,6 +60,7 @@ class Post:
             note_data = {
             "id": row_from_db['note.id'],
             "note": row_from_db['note'],
+            "date":row_from_db['date'],
             "created_at": row_from_db['note.created_at'],
             "updated_at": row_from_db['note.updated_at']
         }
@@ -76,6 +78,7 @@ class Post:
                 "id":row_from_db["user.id"],
                 "first_name":row_from_db["first_name"],
                 "last_name":row_from_db["last_name"],
+                "role":row_from_db["role"],
                 "email":row_from_db["email"],
                 "password":row_from_db["password"],
                 "created_at":row_from_db["user.created_at"],
